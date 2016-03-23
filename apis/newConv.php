dac57dbeb2ea9efb260229bb2c8e420d075badb8
sharmax <abhishake1204@hotmail.com>
2016-03-23 12:57:56 +0530

    some more changes

--- apis/newConv.php
+++ apis/newConv.php
@@ -15,15 +15,16 @@
 						$cat = $categories[intval($_GET['cat'])-1];
 						$id = mysql_num_rows(mysql_query("select * from communications"));
 						$id = $id + 1;
-						$conversations[$id] = array();
-						$conversations[$id]['bitsid'] = $user;
-						$conversations[$id]['category'] = $userinfo['category'];
-						$conversations[$id]['cat'] = $cat;
-						$conversations[$id]['name'] = $userinfo['name'];
-						$conversations[$id]['date'] = date("d/m/Y");
-						$conversations[$id]['time'] = date('h:i A');
-						$conversations[$id]['admins'] = "";
-						$conversations[$id]['status'] = "open";
+						$conversations["data"] = array();
+						$conversations["data"][$id] = array();
+						$conversations["data"][$id]['bitsid'] = $user;
+						$conversations["data"][$id]['category'] = $userinfo['category'];
+						$conversations["data"][$id]['cat'] = $cat;
+						$conversations["data"][$id]['name'] = $userinfo['name'];
+						$conversations["data"][$id]['date'] = date("d/m/Y");
+						$conversations["data"][$id]['time'] = date('h:i A');
+						$conversations["data"][$id]['admins'] = "";
+						$conversations["data"][$id]['status'] = "open";
 						if($cat == "breview"){
 							$title = $_GET['inputArray'][0];
 							$author = $_GET['inputArray'][1];
@@ -31,26 +32,26 @@
 								$conversations['message'] = "Book Review has already been submitted.";
 							}
 							else{
-								$conversations[$id]['topic'] = "I Just Read: ".$title;
+								$conversations["data"][$id]['topic'] = "I Just Read: ".$title;
 								$comms = "Title: ".$title."<br />"."Author: ".$author."<br />"."Review: ".$_GET['inputArray'][2];
 							}
 						}
 						else if($cat == "grieve"){
 							if($userinfo['category']!="Admin"){
-								$conversations[$id]['topic'] = "Not Happy with: ".$_GET['inputArray'][0];
+								$conversations["data"][$id]['topic'] = "Not Happy with: ".$_GET['inputArray'][0];
 								$comms = $_GET['inputArray'][1];
 							}
 						}
 						else if($cat == "ill"){
 							$nature = $_GET['inputArray'][0];
 							if($nature=="Books"){
-								$conversations[$id]['topic'] = "Book Not Found: ".$_GET['inputArray'][1];
+								$conversations["data"][$id]['topic'] = "Book Not Found: ".$_GET['inputArray'][1];
 								$comms = "Title: ".$_GET['inputArray'][1]."<br />"."Author: ".$_GET['inputArray'][2]."<br />"."Accession No.: ".$_GET['inputArray'][3];
 							}
 							else{
 								$month = $_GET['inputArray'][2];
 								$year = $_GET['inputArray'][3];
-								$conversations[$id]['topic'] = "Journal Not Found: ".$title;
+								$conversations["data"][$id]['topic'] = "Journal Not Found: ".$title;
 								$comms = "Journal/Magazine Name: ".$title."<br />"."Month: ".$month."<br />"."Year.: ".$year;
 							}
 						}
@@ -61,25 +62,25 @@
 								$comms .= "<tr><td>".($i+1)."</td><td>".$_GET['inputArray'][5*$i]."</td><td>".$_GET['inputArray'][5*$i + 1]."</td><td>".$_GET['inputArray'][5*$i + 2]."</td><td>".$_GET['inputArray'][5*$i + 3]."</td><td>".$_GET['inputArray'][5*$i + 4]."</td></tr>";
 							}
 							$comms .= "</table>";
-							$conversations[$id]['topic'] = "Books Recommended";
+							$conversations["data"][$id]['topic'] = "Books Recommended";
 						}
 						else if($cat == "feedback"){
-							$conversations[$id]['topic'] = "".$_GET['inputArray'][0];
+							$conversations["data"][$id]['topic'] = "".$_GET['inputArray'][0];
 							$comms = $_GET['inputArray'][1];
 						}
 						else{
-							$conversations[$id]['topic'] = "Database Not Accessible! : ".$_GET['inputArray'][0];
+							$conversations["data"][$id]['topic'] = "Database Not Accessible! : ".$_GET['inputArray'][0];
 							$comms = "Name of Database: ".$_GET['inputArray'][0]."<br />"."Title of Journal: ".$_GET['inputArray'][1]."<br />"."Location where access of denied: ".$_GET['inputArray'][2];
 						}
 						if($x = mysql_query("select * from automessage  where type='".$cat."' and status='first'")){
 							$auto = mysql_fetch_array($x);
-							$comm = $userinfo['name']."(Date-".$conversations[$id]['date'].",Time-".$conversations[$id]['time'].")| ".$comms." // From Library(Date-".$conversations[$id]['date'].",Time-".$conversations[$id]['time'].")| ".$auto['text']." //";
+							$comm = $userinfo['name']."(Date-".$conversations["data"][$id]['date'].",Time-".$conversations["data"][$id]['time'].")| ".$comms." // From Library(Date-".$conversations["data"][$id]['date'].",Time-".$conversations["data"][$id]['time'].")| ".$auto['text']." //";
 						}
 						else{
-							$comm = $userinfo['name']."(Date-".$conversations[$id]['date'].",Time-".$conversations[$id]['time'].")| ".$comms." //";
+							$comm = $userinfo['name']."(Date-".$conversations["data"][$id]['date'].",Time-".$conversations["data"][$id]['time'].")| ".$comms." //";
 						}
-						$conversations[$id]['talk'] = $comm;
-						if(mysql_query("insert into communications (bitsid,topic,comms,status,admins,id,date,cat,time) values ('".$user."','".$conversations[$id]['topic']."','".$comm."','open','','".$id."','".$conversations[$id]['date']."','".$cat."','".$conversations[$id]['time']."')")){
+						$conversations["data"][$id]['talk'] = $comm;
+						if(mysql_query("insert into communications (bitsid,topic,comms,status,admins,id,date,cat,time) values ('".$user."','".$conversations["data"][$id]['topic']."','".$comm."','open','','".$id."','".$conversations["data"][$id]['date']."','".$cat."','".$conversations["data"][$id]['time']."')")){
 							$conversations['message'] = 'New conversation created!';
 						}
 						else{
@@ -91,7 +92,7 @@
 					}
 				}
 				else{
-					$conversations['message'] = 'Administrator can\'t submit a new communication';
+					$conversations['err_message'] = 'Administrator can\'t submit a new communication';
 				}
 			}
 			else{
