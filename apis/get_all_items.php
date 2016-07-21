985cec89b6507e2dd6b7f86c6ff06ab112186a9d
Abhishek Sharma <abhishek.sharma@gmail.com>
2016-07-22 02:58:52 +0530

    Lost and Found Items

--- apis/get_all_items.php
+++ apis/get_all_items.php
@@ -1,23 +1,40 @@
 <?php
-$response = array();
-require_once __DIR__ . '/db_connect.php';
-$db = new DB_CONNECT();
-$result = mysql_query("SELECT * FROM lfms") or die(mysql_error());
-if ($result && mysql_num_rows($result) > 0) {
-    $response["items"] = array();
-    while ($row = mysql_fetch_array($result)) {
-        $item = array();
-        $item["sno"] = $row["sno"];
-        $item["particulars"] = $row["particulars"];
-        $item["Brand_Name"] = $row["Brand_Name"];
-        $item["Found_By"] = $row["Found_By"];
-        array_push($response["items"], $item);
+  require_once __DIR__ . '/db_connect.php';
+  $db = new DB_CONNECT();
+  $response['err_message'] = '';
+  $response['message'] = '';
+  $response["success"] = 0;
+  if(isset($_GET['username'],$_GET['password'])){
+    $user = $_GET['username'];
+    $password = $_GET['password'];
+    if($userinfo = mysql_fetch_array(mysql_query("select * from users where bitsid='".$user."'"))){
+      if(sha1($password) == $userinfo['password']){
+        $result = mysql_query("SELECT * FROM lfms where status <> 2 ORDER BY sno DESC");
+        if($result && mysql_num_rows($result) > 0){
+          $response["data"] = array();
+          while($row = mysql_fetch_array($result)){
+              $response["data"][$row["sno"]] = array();
+              $response["data"][$row["sno"]]["particulars"] = $row["particulars"];
+              $response["data"][$row["sno"]]["brand"] = $row["Brand_Name"];
+              $response["data"][$row["sno"]]["found_by"] = $row["Found_By"];
+              $response["data"][$row["sno"]]["status"] = $row["status"];
+          }
+          $response["success"] = 1;
+        } 
+        else {
+          $response["message"] = "No Items found";
+        }
+      }
+      else{
+        $response['err_message'] = 'Invalid Password!';
+      }
     }
-    $response["success"] = 1;
-    echo json_encode($response);
-} else {
-    $response["success"] = 0;
-    $response["message"] = "No Items found";
-    echo json_encode($response);
-}
+    else{
+      $response['err_message'] = "Invalid Username!";	
+    }
+  }
+  else{
+    $response['err_message'] = "Incomplete Parameters!";
+  }
+  echo json_encode($response);
 ?>
--- apis/update_item.php
+++ apis/update_item.php
@@ -1,29 +1,36 @@
 <?php
-$response = array();
-if (isset($_POST['sno']) && isset($_POST['name'])) {
-    
-    $sno = $_POST['sno'];
-    $name = $_POST['name'];
-   
-    require_once __DIR__ . '/db_connect.php';
-    $db = new DB_CONNECT();
-    $result = mysql_query("UPDATE lfms SET mobile = '$name', status='0' WHERE sno = $sno");
-    if ($result) {
-        // successfully updated
-        $response["success"] = 1;
-        $response["message"] = "Item successfully updated.";
-        
-        // echoing JSON response
-        echo json_encode($response);
-    } else {
-        
+  require_once __DIR__ . '/db_connect.php';
+  $db = new DB_CONNECT();
+  $response['err_message'] = '';
+  $response['message'] = '';
+  $response["success"] = 0;
+  if(isset($_GET['username'],$_GET['password'])){
+    $user = $_GET['username'];
+    $password = $_GET['password'];
+    if($userinfo = mysql_fetch_array(mysql_query("select * from users where bitsid='".$user."'"))){
+      if(sha1($password) == $userinfo['password']){
+        if (isset($_GET['sno']) && isset($_GET['name'])) {
+          $sno = $_GET['sno'];
+          $name = $_GET['name'];
+          $result = mysql_query("UPDATE lfms SET mobile = '$name', status='0' WHERE sno = $sno");
+          if ($result) {
+              $response["success"] = 1;
+              $response["message"] = "Item successfully updated.";
+          }
+        } else {
+            $response["message"] = "Required field(s) is missing";;
+        }
+      }
+      else{
+        $response['err_message'] = 'Invalid Password!';
+      }
     }
-} else {
-    // required field is missing
-    $response["success"] = 0;
-    $response["message"] = "Required field(s) is missing";
-
-    // echoing JSON response
-    echo json_encode($response);
-}
+    else{
+      $response['err_message'] = "Invalid Username!";	
+    }
+  }
+  else{
+    $response['err_message'] = "Incomplete Parameters!";
+  }
+  echo json_encode($response);
 ?>
